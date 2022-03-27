ede79bee17cef73f8be8e6ee5cc236d71a9597c9
admin <admin@localhost.org>
2022-03-27 21:11:52 +0300

    Minor fix

--- bundle.py
+++ bundle.py
@@ -10,6 +10,7 @@ def CalculateChecksum(header):
     return crc32(header) & 0xffffffff # XXX: Python bug 1202
 
 FILE_PATH = "/home/ddd/VMware-Player-12.5.9-7535481.x86_64.bundle"
+FILE_PATH = "/home/ddd/VMware-Player-14.1.1-7528167.x86_64.bundle"
 
 source = open(FILE_PATH, 'rb')
 
@@ -92,7 +93,7 @@ with open(new_file_path, 'wb') as new_file:
     preoffset = launcher_patched_size
     launcherSize = launcher_patched_size
     payloadOffset = launcher_patched_size
-    manifestOffset = payloadOffset + payloadSize
+    manifestOffset = payloadOffset + payloadSize + 1
     dataOffset = manifestOffset + manifestSize
     new_footer = struct.pack(FOOTER_FORMAT, \
         dataSize, dataOffset, manifestSize, manifestOffset, payloadSize, payloadOffset, \
