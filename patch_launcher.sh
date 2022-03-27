19da12f39051fa8db51cfe40d3c06243f6f84846
admin <admin@localhost.org>
2022-03-27 20:46:14 +0300

    Some fixes

--- patch_launcher.sh
+++ patch_launcher.sh
@@ -7,10 +7,10 @@ cat >> launcher_patched.sh <<- EOM
    PYSO=\$VMIS_TEMP/install/vmware-installer/python/lib/lib-dynload/
    for so in \${PYSO}/*.so; do
       echo "Patch \$so"
-      patchelf --replace-needed libpython2.7.so.1.0 ${PYTHON_SO} \$so
-      patchelf --replace-needed libncursesw.so.5 ${NCURSES_SO} \$so
-      patchelf --replace-needed libsqlite3.so.0 ${SQLITE_SO} \$so
-      patchelf --replace-needed libz.so.1 ${ZLIB_SO} \$so
+      patchelf --replace-needed libpython2.7.so.1.0 ${PYTHON_SO}/lib/libpython2.7.so.1.0  \$so
+      patchelf --replace-needed libncursesw.so.5 ${NCURSES_SO}/lib/libncursesw.so.5  \$so
+      patchelf --replace-needed libsqlite3.so.0 ${SQLITE_SO}/lib/libsqlite3.so.0  \$so
+      patchelf --replace-needed libz.so.1 ${ZLIB_SO}/lib/libz.so.1  \$so
    done
    echo "Patch done"
 EOM
