adb74c65f0cddaa173a6a2f044f46a9521ec8e18
admin <admin@localhost.org>
2022-04-03 13:53:09 +0300

    More functionality for find_lib

--- find_lib.sh
+++ find_lib.sh
@@ -1,8 +1,15 @@
 SRC=result/
-libs=$(find $SRC -iname "*.so*" | xargs ldd 2> /dev/null | grep -i "not found" | sort | uniq | awk -s '{ print $1; }' | grep -v "\/")
-#echo $libs
-#DEST=${placeholder "out"}/usr/lib/vmware/lib
+files=$(find $SRC -iname "*")
+bins=
+for f in $files; do
+    if [ -n "$(file $f | grep -i elf)" ]; then
+        bins="$bins $f"
+    fi
+done
+#libs=$(find $SRC -iname "*.so*" | xargs ldd 2> /dev/null | grep -i "not found" | sort | uniq | awk -s '{ print $1; }' | grep -v "\/")
+libs=$(echo $bins | xargs ldd 2> /dev/null | grep -i "not found" | sort | uniq | awk -s '{ print $1; }' | grep -v "\/")
 DEST=/
+rm patches
 for lib in $libs; do
     res=$(find result/ -iname "$lib")
     if [ -z "$res" ]; then
@@ -19,6 +26,17 @@ for lib in $libs; do
             continue
         fi
         echo " - $libPath"
-        echo "patchelf --replace-needed $lib $DEST/$libPath \$elf"
+        echo "patchelf --replace-needed $lib $DEST/$libPath" >> patches
     fi
+done
+
+rm bin_patches
+for bin in $bins; do
+    ulibs=$(ldd $bin 2> /dev/null | grep -i "not found" | sort | uniq | awk -s '{ print $1; }' | grep -v "\/")
+    for ulib in $ulibs; do
+        p=$(cat patches | grep "$ulib")
+        if [ -n "$p" ]; then
+            echo "$p $bin" >> bin_patches
+        fi
+    done
 done
