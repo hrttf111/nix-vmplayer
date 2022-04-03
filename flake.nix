3b5176019dc4fb56dd49f38eae6bfdb2e6e89ec8
hrttf111 <33761444+hrttf111@users.noreply.github.com>
2022-04-03 15:25:53 +0300

    Patching works

--- flake.nix
+++ flake.nix
@@ -136,6 +136,8 @@
         xorg.libX11 xorg.libXext xorg.libSM xorg.libICE xorg.libXi xorg.libXtst
         libjpeg libpng12
         cups
+        util-linux
+        libuuid
       ];
 
       configurePhase = ''
@@ -208,13 +210,13 @@
           $bundleSource/vmware-player-app/extras/thnucups \
           "$pkgdir/usr/lib/cups/filter"
 
-        install -Dm 644 "$bundleSource/vmware-player-app/doc/LearnMore.txt" "$pkgdir/usr/share/licenses/$pkgname/Privacy.txt"
+        #install -Dm 644 "$bundleSource/vmware-player-app/doc/LearnMore.txt" "$pkgdir/usr/share/licenses/$pkgname/Privacy.txt"
         install -Dm 644 "$pkgdir/usr/lib/vmware-ovftool/vmware.eula" "$pkgdir/usr/share/licenses/$pkgname/VMware OVF Tool - EULA.txt"
         rm "$pkgdir/usr/lib/vmware-ovftool"/{vmware.eula,vmware-eula.rtf,open_source_licenses.txt,manifest.xml}
 
         install -Dm 644 "$bundleSource/vmware-vmx/etc/modprobe.d/modprobe-vmware-fuse.conf" "$pkgdir/etc/modprobe.d/vmware-fuse.conf"
 
-        install -Dm 644 $bundleSource/vmware-player-app/lib/isoimages/tools-key.pub "$pkgdir/usr/lib/vmware/isoimages/tools-key.pub"
+        #install -Dm 644 $bundleSource/vmware-player-app/lib/isoimages/tools-key.pub "$pkgdir/usr/lib/vmware/isoimages/tools-key.pub"
 
         install -Dm 644 $bundleSource/vmware-vmx/extra/modules.xml "$pkgdir"/usr/lib/vmware/modules/modules.xml
         install -Dm 644 $bundleSource/vmware-installer/bootstrap "$pkgdir"/etc/vmware-installer/bootstrap
@@ -225,7 +227,6 @@
           "$pkgdir/usr/bin"/* \
           "$pkgdir/usr/lib/vmware/bin"/* \
           "$pkgdir/usr/lib/vmware/setup"/* \
-          "$pkgdir/usr/lib/vmware/lib"/{wrapper-gtk24.sh,libgksu2.so.0/gksu-run-helper} \
           "$pkgdir/usr/lib/vmware-ovftool"/{ovftool,ovftool.bin} \
           "$pkgdir/usr/lib/vmware-installer/$vmware_installer_version"/{vmware-installer,vmis-launcher} \
           "$pkgdir/usr/lib/cups/filter"/*
@@ -237,7 +238,6 @@
 
         for link in \
           licenseTool \
-          thnuclnt \
           vmplayer \
           vmware \
           vmware-app-control \
@@ -255,15 +255,15 @@
           ln -s $pkgdir/usr/lib/vmware/bin/appLoader "$pkgdir/usr/lib/vmware/bin/$link"
         done
 
-        for file in \
-          pango/pangorc \
-          pango/pango.modules \
-          pango/pangox.aliases \
-          gtk-2.0/gdk-pixbuf.loaders \
-          gtk-2.0/gtk.immodules
-        do
-          sed -i 's,@@LIBCONF_DIR@@,/usr/lib/vmware/libconf,g' "$pkgdir/usr/lib/vmware/libconf/etc/$file"
-        done
+#        for file in \
+#          pango/pangorc \
+#          pango/pango.modules \
+#          pango/pangox.aliases \
+#          gtk-2.0/gdk-pixbuf.loaders \
+#          gtk-2.0/gtk.immodules
+#        do
+#          sed -i 's,@@LIBCONF_DIR@@,/usr/lib/vmware/libconf,g' "$pkgdir/usr/lib/vmware/libconf/etc/$file"
+#        done
 
         # create symlinks (replicate installer) - misc
         ln -s $out/lib/vmware/icu $out/etc/vmware/icu
@@ -285,10 +285,15 @@
         install -m644 $src/vmware-config-bootstrap "$out"/etc/vmware/bootstrap
         install -Dm 644 $src/vmware-config "$out"/etc/vmware/config
 
+        echo "patchelf --replace-needed libcups.so.2 ${pkgs.cups.out}/lib/libcups.so.2" > patches
+        #echo "patchelf --replace-needed libuuid.so.1 ${pkgs.util-linux.out}/lib/libuuid.so.1" >> patches
+        echo "patchelf --replace-needed libuuid.so.1 ${pkgs.libuuid.lib}/lib/libuuid.so.1" >> patches
+        ./find_lib.sh "$pkgdir"
+        echo "Start patching"
+        ./bin_patches
+
         function patchElf() {
           elf=$1
-          echo "Patch $elf"
-          #patchelf --set-interpreter ${pkgs.glibc}/lib64/ld-linux-x86-64.so.2 $elf
           patchelf --replace-needed libpython2.7.so.1.0 ${pkgs.python27}/lib/libpython2.7.so.1.0 $elf
           patchelf --replace-needed libncursesw.so.5 ${pkgs.ncurses5}/lib/libncursesw.so.5 $elf
           patchelf --replace-needed libsqlite3.so.0 ${pkgs.sqlite.out}/lib/libsqlite3.so.0 $elf
@@ -302,116 +307,26 @@
           patchelf --replace-needed libcups.so.2 ${pkgs.cups.out}/lib/libcups.so.2 $elf
           patchelf --replace-needed libXi.so.6 ${pkgs.xorg.libXi}/lib/libXi.so.6 $elf
           patchelf --replace-needed libXtst.so.6 ${pkgs.xorg.libXtst}/lib/libXtst.so.6 $elf
-
-          patchelf --replace-needed libatk-1.0.so.0 ${placeholder "out"}/usr/lib/vmware/lib/libatk-1.0.so.0/libatk-1.0.so.0 $elf
-          patchelf --replace-needed libatkmm-1.6.so.1 ${placeholder "out"}/usr/lib/vmware/lib/libatkmm-1.6.so.1/libatkmm-1.6.so.1 $elf
-          patchelf --replace-needed libatspi.so.0 ${placeholder "out"}/usr/lib/vmware/lib/libatspi.so.0/libatspi.so.0 $elf
-          patchelf --replace-needed libbasichttp.so ${placeholder "out"}/usr/lib/vmware/lib/libbasichttp.so/libbasichttp.so $elf
-          patchelf --replace-needed libcairomm-1.0.so.1 ${placeholder "out"}/usr/lib/vmware/lib/libcairomm-1.0.so.1/libcairomm-1.0.so.1 $elf
-          patchelf --replace-needed libcairo.so.2 ${placeholder "out"}/usr/lib/vmware/lib/libcairo.so.2/libcairo.so.2 $elf
-          patchelf --replace-needed libcds.so ${placeholder "out"}/usr/lib/vmware/lib/libcds.so/libcds.so $elf
-          patchelf --replace-needed libcrypto.so.1.0.1 ${placeholder "out"}/usr/lib/vmware/lib/libcrypto.so.1.0.1/libcrypto.so.1.0.1 $elf
-          patchelf --replace-needed libcurl.so.4 ${placeholder "out"}/usr/lib/vmware/lib/libcurl.so.4/libcurl.so.4 $elf
-          patchelf --replace-needed libdbus-1.so.3 ${placeholder "out"}/usr/lib/vmware/lib/libdbus-1.so.3/libdbus-1.so.3 $elf
-          patchelf --replace-needed libexpat.so.0 ${placeholder "out"}/usr/lib/vmware-ovftool/libexpat.so.0 $elf
-          patchelf --replace-needed libfontconfig.so.1 ${placeholder "out"}/usr/lib/vmware/lib/libfontconfig.so.1/libfontconfig.so.1 $elf
-          patchelf --replace-needed libfreetype.so.6 ${placeholder "out"}/usr/lib/vmware/lib/libfreetype.so.6/libfreetype.so.6 $elf
-
-          # for so in $(cat 5); do echo "patchelf --replace-needed $(basename $so) \${placeholder "out"}$so \$elf"; done
-          patchelf --replace-needed libatk-1.0.so.0 ${placeholder "out"}/usr/lib/vmware/lib/libatk-1.0.so.0/libatk-1.0.so.0 $elf
-          patchelf --replace-needed libatkmm-1.6.so.1 ${placeholder "out"}/usr/lib/vmware/lib/libatkmm-1.6.so.1/libatkmm-1.6.so.1 $elf
-          patchelf --replace-needed libatspi.so.0 ${placeholder "out"}/usr/lib/vmware/lib/libatspi.so.0/libatspi.so.0 $elf
-          patchelf --replace-needed libbasichttp.so ${placeholder "out"}/usr/lib/vmware/lib/libbasichttp.so/libbasichttp.so $elf
-          patchelf --replace-needed libcairomm-1.0.so.1 ${placeholder "out"}/usr/lib/vmware/lib/libcairomm-1.0.so.1/libcairomm-1.0.so.1 $elf
-          patchelf --replace-needed libcairo.so.2 ${placeholder "out"}/usr/lib/vmware/lib/libcairo.so.2/libcairo.so.2 $elf
-          patchelf --replace-needed libcds.so ${placeholder "out"}/usr/lib/vmware/lib/libcds.so/libcds.so $elf
-          patchelf --replace-needed libcrypto.so.1.0.1 ${placeholder "out"}/usr/lib/vmware/lib/libcrypto.so.1.0.1/libcrypto.so.1.0.1 $elf
-          patchelf --replace-needed libcurl.so.4 ${placeholder "out"}/usr/lib/vmware/lib/libcurl.so.4/libcurl.so.4 $elf
-          patchelf --replace-needed libdbus-1.so.3 ${placeholder "out"}/usr/lib/vmware/lib/libdbus-1.so.3/libdbus-1.so.3 $elf
-          patchelf --replace-needed libexpat.so.0 ${placeholder "out"}/usr/lib/vmware-ovftool/libexpat.so.0 $elf
-          patchelf --replace-needed libfontconfig.so.1 ${placeholder "out"}/usr/lib/vmware/lib/libfontconfig.so.1/libfontconfig.so.1 $elf
-          patchelf --replace-needed libfreetype.so.6 ${placeholder "out"}/usr/lib/vmware/lib/libfreetype.so.6/libfreetype.so.6 $elf
-          patchelf --replace-needed libfuse.so.2 ${placeholder "out"}/usr/lib/vmware/lib/libfuse.so.2/libfuse.so.2 $elf
-          patchelf --replace-needed libgailutil.so.18 ${placeholder "out"}/usr/lib/vmware/lib/libgailutil.so.18/libgailutil.so.18 $elf
-          patchelf --replace-needed libgck.so.0 ${placeholder "out"}/usr/lib/vmware/lib/libgck.so.0/libgck.so.0 $elf
-          patchelf --replace-needed libgcr.so.0 ${placeholder "out"}/usr/lib/vmware/lib/libgcr.so.0/libgcr.so.0 $elf
-          patchelf --replace-needed libgcrypt.so.11 ${placeholder "out"}/usr/lib/vmware/lib/libgcrypt.so.11/libgcrypt.so.11 $elf
-          patchelf --replace-needed libgdkmm-2.4.so.1 ${placeholder "out"}/usr/lib/vmware/lib/libgdkmm-2.4.so.1/libgdkmm-2.4.so.1 $elf
-          patchelf --replace-needed libgdk_pixbuf-2.0.so.0 ${placeholder "out"}/usr/lib/vmware/lib/libgdk_pixbuf-2.0.so.0/libgdk_pixbuf-2.0.so.0 $elf
-          patchelf --replace-needed libgdk-x11-2.0.so.0 ${placeholder "out"}/usr/lib/vmware/lib/libgdk-x11-2.0.so.0/libgdk-x11-2.0.so.0 $elf
-          patchelf --replace-needed libgio-2.0.so.0 ${placeholder "out"}/usr/lib/vmware/lib/libgio-2.0.so.0/libgio-2.0.so.0 $elf
-          patchelf --replace-needed libgiomm-2.4.so.1 ${placeholder "out"}/usr/lib/vmware/lib/libgiomm-2.4.so.1/libgiomm-2.4.so.1 $elf
-          patchelf --replace-needed libgksu2.so.0 ${placeholder "out"}/usr/lib/vmware/lib/libgksu2.so.0/libgksu2.so.0 $elf
-          patchelf --replace-needed libglib-2.0.so.0 ${placeholder "out"}/usr/lib/vmware/lib/libglib-2.0.so.0/libglib-2.0.so.0 $elf
-          patchelf --replace-needed libglibmm-2.4.so.1 ${placeholder "out"}/usr/lib/vmware/lib/libglibmm-2.4.so.1/libglibmm-2.4.so.1 $elf
-          patchelf --replace-needed libgmodule-2.0.so.0 ${placeholder "out"}/usr/lib/vmware/lib/libgmodule-2.0.so.0/libgmodule-2.0.so.0 $elf
-          patchelf --replace-needed libgobject-2.0.so.0 ${placeholder "out"}/usr/lib/vmware/lib/libgobject-2.0.so.0/libgobject-2.0.so.0 $elf
-          patchelf --replace-needed libgpg-error.so.0 ${placeholder "out"}/usr/lib/vmware/lib/libgpg-error.so.0/libgpg-error.so.0 $elf
-          patchelf --replace-needed libgthread-2.0.so.0 ${placeholder "out"}/usr/lib/vmware/lib/libgthread-2.0.so.0/libgthread-2.0.so.0 $elf
-          patchelf --replace-needed libgtkmm-2.4.so.1 ${placeholder "out"}/usr/lib/vmware/lib/libgtkmm-2.4.so.1/libgtkmm-2.4.so.1 $elf
-          patchelf --replace-needed libgtk-x11-2.0.so.0 ${placeholder "out"}/usr/lib/vmware/lib/libgtk-x11-2.0.so.0/libgtk-x11-2.0.so.0 $elf
-          patchelf --replace-needed libgtop-2.0.so.7 ${placeholder "out"}/usr/lib/vmware/lib/libgtop-2.0.so.7/libgtop-2.0.so.7 $elf
-          patchelf --replace-needed libgvmomi.so ${placeholder "out"}/usr/lib/vmware/lib/libgvmomi.so/libgvmomi.so $elf
-          patchelf --replace-needed libpango-1.0.so.0 ${placeholder "out"}/usr/lib/vmware/lib/libpango-1.0.so.0/libpango-1.0.so.0 $elf
-          patchelf --replace-needed libpangocairo-1.0.so.0 ${placeholder "out"}/usr/lib/vmware/lib/libpangocairo-1.0.so.0/libpangocairo-1.0.so.0 $elf
-          patchelf --replace-needed libpangoft2-1.0.so.0 ${placeholder "out"}/usr/lib/vmware/lib/libpangoft2-1.0.so.0/libpangoft2-1.0.so.0 $elf
-          patchelf --replace-needed libpangomm-1.4.so.1 ${placeholder "out"}/usr/lib/vmware/lib/libpangomm-1.4.so.1/libpangomm-1.4.so.1 $elf
-          patchelf --replace-needed libpangox-1.0.so.0 ${placeholder "out"}/usr/lib/vmware/lib/libpangox-1.0.so.0/libpangox-1.0.so.0 $elf
-          patchelf --replace-needed libpixman-1.so.0 ${placeholder "out"}/usr/lib/vmware/lib/libpixman-1.so.0/libpixman-1.so.0 $elf
-          patchelf --replace-needed librsvg-2.so.2 ${placeholder "out"}/usr/lib/vmware/lib/librsvg-2.so.2/librsvg-2.so.2 $elf
-          patchelf --replace-needed libsigc-2.0.so.0 ${placeholder "out"}/usr/lib/vmware/lib/libsigc-2.0.so.0/libsigc-2.0.so.0 $elf
-          patchelf --replace-needed libssl.so.1.0.1 ${placeholder "out"}/usr/lib/vmware/lib/libssl.so.1.0.1/libssl.so.1.0.1 $elf
-          patchelf --replace-needed libssoclient.so ${placeholder "out"}/usr/lib/vmware-ovftool/libssoclient.so $elf
-          patchelf --replace-needed libstdc++.so.6 ${placeholder "out"}/usr/lib/vmware/lib/libstdc++.so.6/libstdc++.so.6 $elf
-          patchelf --replace-needed libview.so.3 ${placeholder "out"}/usr/lib/vmware/lib/libview.so.3/libview.so.3 $elf
-          patchelf --replace-needed libvmacore.so ${placeholder "out"}/usr/lib/vmware-ovftool/libvmacore.so $elf
-          patchelf --replace-needed libvmomi.so ${placeholder "out"}/usr/lib/vmware-ovftool/libvmomi.so $elf
-          patchelf --replace-needed libvmwarebase.so ${placeholder "out"}/usr/lib/vmware/lib/libvmwarebase.so/libvmwarebase.so $elf
-          patchelf --replace-needed libvmwareui.so ${placeholder "out"}/usr/lib/vmware/lib/libvmwareui.so/libvmwareui.so $elf
-          patchelf --replace-needed libXau.so.6 ${placeholder "out"}/usr/lib/vmware/lib/libXau.so.6/libXau.so.6 $elf
-          patchelf --replace-needed libXcomposite.so.1 ${placeholder "out"}/usr/lib/vmware/lib/libXcomposite.so.1/libXcomposite.so.1 $elf
-          patchelf --replace-needed libXcursor.so.1 ${placeholder "out"}/usr/lib/vmware/lib/libXcursor.so.1/libXcursor.so.1 $elf
-          patchelf --replace-needed libXdamage.so.1 ${placeholder "out"}/usr/lib/vmware/lib/libXdamage.so.1/libXdamage.so.1 $elf
-          patchelf --replace-needed libXfixes.so.3 ${placeholder "out"}/usr/lib/vmware/lib/libXfixes.so.3/libXfixes.so.3 $elf
-          patchelf --replace-needed libXft.so.2 ${placeholder "out"}/usr/lib/vmware/lib/libXft.so.2/libXft.so.2 $elf
-          patchelf --replace-needed libXinerama.so.1 ${placeholder "out"}/usr/lib/vmware/lib/libXinerama.so.1/libXinerama.so.1 $elf
-          patchelf --replace-needed libxml2.so.2 ${placeholder "out"}/usr/lib/vmware/lib/libxml2.so.2/libxml2.so.2 $elf
-          patchelf --replace-needed libXrandr.so.2 ${placeholder "out"}/usr/lib/vmware/lib/libXrandr.so.2/libXrandr.so.2 $elf
-          patchelf --replace-needed libXrender.so.1 ${placeholder "out"}/usr/lib/vmware/lib/libXrender.so.1/libXrender.so.1 $elf
         }
 
-        for i in `seq 0 3`; do
-          for lib in $(find $pkgdir -iname "*.so*"); do
-            if [ -n "$(${pkgs.file}/bin/file $lib | grep -i elf)" ]; then
-              patchElf $lib
-            fi
-          done
-        done
-
-        for lib in $(find $pkgdir -iname "*.so*"); do
-          if [ -n "$(${pkgs.file}/bin/file $lib | grep -i elf)" ]; then
-            echo "Show final LDD $elf"
-            ldd $elf
-          fi
-        done
-
-        patchelf --set-interpreter $GLIBC_SO/lib64/ld-linux-x86-64.so.2 $pkgdir/usr/lib/vmware-installer/$vmware_installer_version/vmis-launcher
-        patchelf --set-interpreter $GLIBC_SO/lib64/ld-linux-x86-64.so.2 $pkgdir/usr/lib/vmware/setup/vmware-config
-
-        for filename in $pkgdir/usr/bin/vmw*; do
+#        for elf in $(find $pkgdir -iname "*.so*"); do
+#          if [ -n "$(${pkgs.file}/bin/file $lib | grep -i elf)" ]; then
+#            echo "Show final LDD $elf"
+#            ldd $elf
+#          fi
+#        done
+        for filename in $(cat execs); do
           if [ -n "$(${pkgs.file}/bin/file $filename | grep -i elf)" ]; then
             patchelf --set-interpreter $GLIBC_SO/lib64/ld-linux-x86-64.so.2 $filename
-            patchElf $filename
-            ldd $filename
           fi
         done
-        for filename in $pkgdir/usr/lib/vmware/bin/*; do
-          if [ -n "$(${pkgs.file}/bin/file $filename | grep -i elf)" ]; then
-            patchelf --set-interpreter $GLIBC_SO/lib64/ld-linux-x86-64.so.2 $filename
-            patchElf $filename
-            ldd $filename
-          fi
-        done
+        echo "Stop patching"
+
+        mkdir $pkgdir/usr/share/nix_debug
+        cp patches $pkgdir/usr/share/nix_debug/patches
+        cp bin_patches $pkgdir/usr/share/nix_debug/bin_patches
+        cp not_found $pkgdir/usr/share/nix_debug/not_found
+        cp execs $pkgdir/usr/share/nix_debug/execs
       '';
 
     };
@@ -478,9 +393,12 @@
   {
     #inherit vmware-player;
     overlay = final: prev: {
-      #vmware-player = vmware-player-fhs;
-      vmware-player = vmware-bundle;
+      vmware-player = vmware-player-fhs;
+      #vmware-player = vmware-bundle;
+      #vmware-player = vmware-vmx;
     };
-    defaultPackage.x86_64-linux = vmware-bundle;
+    defaultPackage.x86_64-linux = vmware-player-fhs;
+    #defaultPackage.x86_64-linux = vmware-bundle;
+    #defaultPackage.x86_64-linux = vmware-vmx;
   };
 }
