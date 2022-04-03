094d8f9827370b964d369abc1bce67fab8d2a4f0
admin <admin@localhost.org>
2022-04-03 12:05:09 +0300

    Some changes

--- patch_launcher.sh
+++ patch_launcher.sh
@@ -18,13 +18,22 @@ cat >> launcher_patched.sh <<- EOM
    source $(cat $stdenv/setup | grep "[^ ]*shebangs.sh" -o)
    echo "Patch ELF"
    patchelf --set-interpreter ${GLIBC_SO}/lib64/ld-linux-x86-64.so.2 \$VMIS_TEMP/install/vmware-installer/vmis-launcher
-   PYSO=\$VMIS_TEMP/install/vmware-installer/python/lib/lib-dynload/
-   for so in \${PYSO}/*.so; do
-      echo "Patch \$so"
-      patchelf --replace-needed libpython2.7.so.1.0 ${PYTHON_SO}/lib/libpython2.7.so.1.0  \$so
-      patchelf --replace-needed libncursesw.so.5 ${NCURSES_SO}/lib/libncursesw.so.5  \$so
-      patchelf --replace-needed libsqlite3.so.0 ${SQLITE_SO}/lib/libsqlite3.so.0  \$so
-      patchelf --replace-needed libz.so.1 ${ZLIB_SO}/lib/libz.so.1  \$so
+   patchelf --replace-needed libpython3.9.so.1.0 ${PYTHON_SO}/lib/libpython3.9.so.1.0 \$VMIS_TEMP/install/vmware-installer/vmis-launcher
+   ldd \$VMIS_TEMP/install/vmware-installer/vmis-launcher
+   PYSO=\$VMIS_TEMP/install/vmware-installer/
+   PYTHON_SO1=\$VMIS_TEMP/install/vmware-installer/python/libpython3.9.so.1.0
+   for so in \$(find \$PYSO -iname "*.so*"); do
+      if [ -n "\$(file \$so | grep -i elf)" ]; then
+          echo "Patch \$so"
+          patchelf --replace-needed libncursesw.so.6 ${NCURSES_SO}/lib/libncursesw.so.6  \$so
+          patchelf --replace-needed libreadline.so.6 ${READLINE_SO}/lib/libreadline.so.6  \$so
+          patchelf --replace-needed libsqlite3.so.0 ${SQLITE_SO}/lib/libsqlite3.so.0  \$so
+          patchelf --replace-needed libz.so.1 ${ZLIB_SO}/lib/libz.so.1  \$so
+          patchelf --replace-needed libbz2.so.1.0 ${BZIP_SO}/lib/libbz2.so.1  \$so
+          patchelf --replace-needed liblzma.so.5 ${LZMA_SO}/lib/liblzma.so.5 \$so
+          patchelf --replace-needed libpython3.9.so.1.0 \${PYTHON_SO1}/lib/libpython3.9.so.1.0  \$so
+          ldd \$so
+      fi
    done
    echo "Patch shebang"
    sed -i -e "1 s|.*|#\!$escapedInterpreterLine|" \$VMIS_TEMP/install/vmware-installer/vmware-installer
