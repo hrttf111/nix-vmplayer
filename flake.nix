00206a47a48480f4294e6e49d6d77b542bc6c256
admin <admin@localhost.org>
2022-04-06 21:16:03 +0300

    Cleanup of flake.nix

--- flake.nix
+++ flake.nix
@@ -7,51 +7,25 @@
 
   outputs = { self, nixpkgs }:
   let
-    version12 = "12.5.9";
-    version16 = "16.2.3";
     pkgs = nixpkgs.legacyPackages.x86_64-linux.pkgs;
     kernel = nixpkgs.legacyPackages.x86_64-linux.pkgs.linux_5_15;
 
-    bundle12 = pkgs.fetchurl {
-      url = "http://127.0.0.1/public/VMware-Player-12.5.9-7535481.x86_64.bundle";
-      sha256 = "2a967fe042c87b7a774ba1d5a7d63ee64f34b5220bf286370ca3439fed60487a";
-    };
-
-    bundle16 = pkgs.fetchurl {
-      url = "http://127.0.0.1/public/VMware-Player-Full-16.2.3-19376536.x86_64.bundle";
-      sha256 = "2c320084765b7a4cd79b6a0e834a6d315c4ecd61d0cc053aa7a445a7958738b0";
-    };
-
     mkBundles = import ./bundle pkgs;
-    vmware-bundle-12 = mkBundles.mkBundle12 version12 bundle12;
-    vmware-bundle-16 = mkBundles.mkBundle16 version16 bundle16;
-
     mkVmxs = import ./vmx { inherit pkgs; };
-    vmware-vmx-16 = mkVmxs.mkVmx16 version16 vmware-bundle-16;
-    vmware-vmx-12 = mkVmxs.mkVmx12 version12 vmware-bundle-12;
-
     mkKernels = import ./vmware-kernel.nix (pkgs // { inherit kernel; });
-    vmware-kernel-12 = mkKernels.mkKernel12 version12 vmware-bundle-12;
-    vmware-kernel-16 = mkKernels.mkKernel16 version16 vmware-bundle-16;
 
-    vmware-config-12 = import ./vmware-config.nix {
-      stdenv = pkgs.stdenv;
-      inherit kernel;
-      vmware-vmx = vmware-vmx-12;
-      vmware-kernel = vmware-kernel-12;
-      version = version12;
-    };
-
-    vmware-config-16 = import ./vmware-config.nix {
-      stdenv = pkgs.stdenv;
-      inherit kernel;
-      vmware-vmx = vmware-vmx-16;
-      vmware-kernel = vmware-kernel-16;
-      version = version16;
-    };
-
-    buildFHS = version: vmware-config: vmware-vmx: vmware-kernel:
-      pkgs.buildFHSUserEnv {
+    mkVmwarePlayer = {version, bundle, mkBundle, mkVmx, mkKernel}: rec {
+      vmware-bundle = mkBundle version bundle;
+      vmware-vmx = mkVmx version vmware-bundle;
+      vmware-kernel = mkKernel version vmware-bundle;
+      vmware-config  = import ./vmware-config.nix {
+        stdenv = pkgs.stdenv;
+        inherit kernel;
+        inherit vmware-vmx;
+        inherit vmware-kernel;
+        inherit version;
+      };
+      fhs = pkgs.buildFHSUserEnv {
         name = "vmware-player-fhs-${version}";
 
         targetPkgs = pkgs: with self.pkgs; [
@@ -59,44 +33,49 @@
           vmware-vmx
           vmware-kernel
         ];
-
         profile = ''
           export VMWARE_VMX=${vmware-vmx}
           export VMWARE_CONFIG=${vmware-config}
           export VMWARE_KERNEL=${vmware-kernel}
         '';
-
         extraBuildCommands = ''
         '';
-
         extraInstallCommands = ''
         '';
-
         runScript = "bash -l";
       };
-    vmware-player-fhs12 = buildFHS version12 vmware-config-12 vmware-vmx-12 vmware-kernel-12;
-    vmware-player-fhs16 = buildFHS version16 vmware-config-16 vmware-vmx-16 vmware-kernel-16;
+    };
+
+    vmware-player-12 = mkVmwarePlayer {
+      version = "12.5.9";
+      bundle = pkgs.fetchurl {
+        url = "http://127.0.0.1/public/VMware-Player-12.5.9-7535481.x86_64.bundle";
+        sha256 = "2a967fe042c87b7a774ba1d5a7d63ee64f34b5220bf286370ca3439fed60487a";
+      };
+      mkBundle = mkBundles.mkBundle12;
+      mkVmx = mkVmxs.mkVmx12;
+      mkKernel = mkKernels.mkKernel12;
+    };
+
+    vmware-player-16 = mkVmwarePlayer {
+      version = "16.2.3";
+      bundle = pkgs.fetchurl {
+        url = "http://127.0.0.1/public/VMware-Player-Full-16.2.3-19376536.x86_64.bundle";
+        sha256 = "2c320084765b7a4cd79b6a0e834a6d315c4ecd61d0cc053aa7a445a7958738b0";
+      };
+      mkBundle = mkBundles.mkBundle16;
+      mkVmx = mkVmxs.mkVmx16;
+      mkKernel = mkKernels.mkKernel16;
+    };
   in
   {
-    inherit vmware-bundle-16;
-    inherit vmware-bundle-12;
-    inherit vmware-vmx-16;
-    inherit vmware-vmx-12;
-    inherit vmware-kernel-16;
-    inherit vmware-kernel-12;
-    inherit vmware-player-fhs16;
-    inherit vmware-player-fhs12;
+    inherit vmware-player-12;
+    inherit vmware-player-16;
 
     overlay = final: prev: {
-      vmware-player-12 = vmware-player-fhs12;
-      vmware-player-16 = vmware-player-fhs16;
-      inherit vmware-bundle-16;
-      inherit vmware-bundle-12;
-      inherit vmware-vmx-16;
-      inherit vmware-vmx-12;
-      inherit vmware-kernel-16;
-      inherit vmware-kernel-12;
+      vmware-player-12 = vmware-player-12.fhs;
+      vmware-player-16 = vmware-player-16.fhs;
     };
-    defaultPackage.x86_64-linux = vmware-player-fhs16;
+    defaultPackage.x86_64-linux = vmware-player-16.fhs;
   };
 }
