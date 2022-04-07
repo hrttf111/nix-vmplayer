a95a5b1bc1ed87ea6527228b44a23759e2dd6da9
hrttf111 <33761444+hrttf111@users.noreply.github.com>
2022-04-07 20:06:54 +0300

    Removed old files

--- vmware-config.nix
+++ vmware-config.nix
@@ -11,18 +11,14 @@ stdenv.mkDerivation {
     set -x
     mkdir -p $out/{etc,home,root,usr,bin}
     ln -s ${vmware-vmx}/usr/lib/vmware/bin/vmplayer $out/bin/vmplayer
-    mkdir $out/etc/vmware
-    cp -r ${vmware-vmx}/etc/vmware/* $out/etc/vmware
-    VMWARE_DIR=${vmware-vmx}
-    sed -i "s,@@VMWARE_DIR@@,$VMWARE_DIR," "$out/etc/vmware/bootstrap"
-    sed -i "s,@@VMWARE_DIR@@,$VMWARE_DIR," "$out/etc/vmware/config"
 
+    mkdir $out/etc/vmware
     cp $src/local_conf/home/vmware/preferences $out/etc/vmware/preferences
     echo 'mkdir ~/.vmware; cp /etc/vmware/preferences ~/.vmware/' > $out/bin/copy_pref
     chmod +x $out/bin/copy_pref
     echo "modprobe vmw_vmci; insmod ${vmware-kernel}/lib/modules/${kernel.modDirVersion}/kernel/vmmon.ko" > $out/bin/ins_mods
     chmod +x $out/bin/ins_mods
-    rm $out/etc/vmware/icu
+    #rm $out/etc/vmware/icu
     ln -s ${vmware-vmx}/usr/lib/vmware/icu $out/etc/vmware/icu
   '';
 }
